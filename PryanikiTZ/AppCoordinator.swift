e6a2be37dfc90bccf55ed515dd3f765a67ec88a5
yaroslav2308 <yaroslav.monastyrev@gmail.com>
2022-08-25 16:54:36 +0300

    Remake navigation by using Coordinator pattern

---
+++ PryanikiTZ/AppCoordinator.swift
@@ -0,0 +1,23 @@
+//
+//  AppCoordinator.swift
+//  PryanikiTZ
+//
+//  Created by Yaroslav Monastyrev on 25.08.2022.
+//
+
+import Foundation
+import UIKit
+
+class AppCoordinator {
+    private let window: UIWindow
+    
+    init(window: UIWindow) {
+        self.window = window
+    }
+    
+    func start() {
+        let vc = ViewController()
+        let navigationController = UINavigationController(rootViewController: vc)
+        window.rootViewController = navigationController
+    }
+}
